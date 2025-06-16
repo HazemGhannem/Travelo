8543314a604e7d68ae040747c4be86669dc8f9f9
HazemGhannem <ghannemhazem@gmail.com>
2025-06-16 09:51:20 +0100

    dummy data

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,16 +1,16 @@
 import { UserRole } from "../models/enums/role.enum";
-
+//dummy data for testing 
 const users = [
   {
-    name: "ghannem",
-    email: "ghannem@gmail.com",
+    name: "Jhone",
+    email: "jhone@gmail.com",
     password: "123456",
     role: UserRole.ADMIN,
     createdAt: new Date("2025-06-16T00:00:00Z"),
   },
   {
-    name: "hazem",
-    email: "hazem@gmail.com",
+    name: "Doe",
+    email: "doe@gmail.com",
     password: "123456",
     role: UserRole.USER,
     createdAt: new Date("2025-06-15T00:00:00Z"),
